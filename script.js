35781ec7a33def00b0d9ee3e535ddce77c25fe42
amitkumar2026 <amitkumar829448@gmail.com>
2025-03-22 18:43:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// JavaScript for Flamingo Transworld Website
+
+function sendInquiry() {
+  alert("Inquiry form coming soon!");
+}
+
+function openWhatsApp() {
+  window.location.href = "https://wa.me/919825081806";
+}
+
+function bookAppointment() {
+  alert("Booking appointment feature coming soon!");
+}
+
+function callNumber() {
+  window.location.href = "tel:+919876543221";
+}
+
+// Footer......//
+document.querySelector(".whatsapp").addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
